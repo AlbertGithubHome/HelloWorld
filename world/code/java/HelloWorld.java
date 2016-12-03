53da36cf9236213fde1285fa8e372da0fa953788
AlbertGithubHome <shihengzhen101@163.com>
2016-12-03 17:48:49 +0800

    add java example program

---
+++ world/code/java/HelloWorld.java
@@ -0,0 +1,10 @@
+// This is a program to output 'Hello World!' via Java.
+import java.io.*;
+
+class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
