2f2fb60859ca45e0acfb10883baad1fbf6008400
albert <alberts101@163.com>
2016-12-28 15:09:43 +0800

    add c# example program

---
+++ world/code/c#/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿// This is a program to output 'Hello World!' via C#.
+using System;
+
+namespace NS_HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
