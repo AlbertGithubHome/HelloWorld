e48c8d52cb92ab0bb50a7a189f86dc36b09595e2
albert <alberts101@163.com>
2016-12-28 18:30:31 +0800

    add objective-c example program

---
+++ world/code/objective-c/HelloWorld.m
@@ -0,0 +1,23 @@
+#import <objc/objc.h>
+#import <objc/Object.h>
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+{
+    int n_num;
+}
+- (void)Hello;
+@end
+
+@implementation TestObject
+- (void)Hello {
+    printf("Hello World!\n");
+}
+
+int main()
+{
+    id obj = [[TestObject alloc] init];
+    [obj Hello];
+    return 0;
+}
+@end
